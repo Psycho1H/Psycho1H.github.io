99a9af2d637298292abbdee29c9524ea0971ce49
rami <ramy.za.rr@gmail.com>
2024-09-18 12:47:45 +0200

    move js

---
+++ main.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Video mute button
+  const video = document.getElementById('bg-video');
+  const muteButton = document.getElementById('muteButton');
+  const muteImg = document.getElementById('mute');
+  const unmuteImg = document.getElementById('unmute');
+
+  muteButton.addEventListener('click', () => {
+    if (video.muted) {
+      video.muted = false;
+      unmuteImg.style.display = "block";
+      muteImg.style.display = "none";
+    } else {
+      video.muted = true;
+      muteImg.style.display = "block";
+      unmuteImg.style.display = "none";
+    }
+  });
+  // Set the volume to 70% when the page loads
+  video.volume = 0.07;
+
+
+  // audio visualizer
+  const analyzerContainer = document.getElementById('analyzer');
+  const avatar = document.querySelector('.avatar');
+  new AudioMotionAnalyzer(
+    analyzerContainer,
+    {
+      mode: 3,
+      gradient: 'rainbow',
+      alphaBars: true,
+      overlay: true,
+      bgAlpha: 0,
+      radial: true,
+
+      linearAmplitude: true,
+      linearBoost: 9,
+      showPeaks: true,
+      onCanvasDraw: instance => {
+        instance.radius = .4 + instance.getEnergy();
+        avatar.style.transform = `scale(${1 + instance.getEnergy() * 0.25})`;
+      },
+      source: document.getElementById('bg-video')
+    }
+  );
+});
